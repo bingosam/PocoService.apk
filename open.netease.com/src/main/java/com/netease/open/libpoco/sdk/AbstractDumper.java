b604a8d047c432909c0d261b2dbe8c1ca5947df2
张坤彬 <120905@nd.com>
2024-07-18 10:06:08 +0800

    bug fix：当控件树出现回环结构时，dump出现栈溢出的问题

--- open.netease.com/src/main/java/com/netease/open/libpoco/sdk/AbstractDumper.java
+++ open.netease.com/src/main/java/com/netease/open/libpoco/sdk/AbstractDumper.java
@@ -1,12 +1,17 @@
 package com.netease.open.libpoco.sdk;
 
 import android.annotation.SuppressLint;
+import android.view.accessibility.AccessibilityNodeInfo;
+
+import com.netease.open.libpoco.Node;
 
 import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
 
+import java.util.HashSet;
 import java.util.Map;
+import java.util.Set;
 
 /**
  * Created by adolli on 2017/7/10.
@@ -21,10 +26,11 @@ public abstract class AbstractDumper implements IDumper<AbstractNode> {
     }
 
     public JSONObject dumpHierarchy(boolean onlyVisibleNode) throws JSONException {
-        return this.dumpHierarchyImpl(this.getRoot(), onlyVisibleNode);
+        Set<AccessibilityNodeInfo> trace = new HashSet<>();
+        return this.dumpHierarchyImpl(trace, this.getRoot(), onlyVisibleNode);
     }
 
-    public JSONObject dumpHierarchyImpl(AbstractNode node, boolean onlyVisibleNode) throws JSONException {
+    public JSONObject dumpHierarchyImpl(Set<AccessibilityNodeInfo> trace, AbstractNode node, boolean onlyVisibleNode) throws JSONException {
         if (node == null) {
             // return if still null
             return null;
@@ -38,13 +44,19 @@ public abstract class AbstractDumper implements IDumper<AbstractNode> {
         JSONObject result = new JSONObject();
         JSONArray children = new JSONArray();
         for (AbstractNode child : node.getChildren()) {
+            if (child instanceof Node) {
+                if (trace.contains(((Node) child).node)) {
+                    continue;
+                }
+                trace.add(((Node) child).node);
+            }
             if (!onlyVisibleNode || (boolean) child.getAttr("visible")) {
-                children.put(this.dumpHierarchyImpl(child, onlyVisibleNode));
+                children.put(this.dumpHierarchyImpl(trace, child, onlyVisibleNode));
             }
             else if(String.valueOf(child.getAttr("type")).endsWith(".WebView")){
                 // 这个改动主要是一些WebView如果使用了tbs引擎，会因为isVisibleToUser返回false而导致无法获取节点
                 // 测试时使用了腾讯自选股app的基金页面进行复现
-                children.put(this.dumpHierarchyImpl(child, false));
+                children.put(this.dumpHierarchyImpl(trace, child, false));
             }
         }
         if (children.length() > 0) {
